e8c93c5d943bdeccccd968954d56f07c2c96fccf
ErnestoRomeroG <ernestosmitch@gmail.com>
2024-04-06 09:11:02 -0600

    colores

--- appJaumave/lib/paginas/contactos.dart
+++ appJaumave/lib/paginas/contactos.dart
@@ -224,7 +224,8 @@ class Usercontactos extends StatelessWidget {
                         style: ElevatedButton.styleFrom(
                           //shape: const CircleBorder(),
                           padding: const EdgeInsets.all(20),
-                          backgroundColor: Colors.black, // <-- Button color
+                          backgroundColor:
+                              Colors.green[400], // <-- Button color
                           foregroundColor: Colors.red, // <-- Splash color
                         ),
                         child: Link(
