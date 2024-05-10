f47f0a0ec0a48d85c72a20afe5d98778e3a577f0
Jose Arturo Villanueva Gonzalez <162375114+Arturox21@users.noreply.github.com>
2024-05-09 22:31:51 -0500

    Update mariposa.dart

--- appJaumave/lib/paginas/mariposa.dart
+++ appJaumave/lib/paginas/mariposa.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:model_viewer_plus/model_viewer_plus.dart';
+import 'package:mave/paginas/negocios.dart';
 
 class mariposa extends StatelessWidget {
   const mariposa({super.key});
 
-  @override
+  Widget build(BuildContext context) => Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.black),
+        centerTitle: true,
+        elevation: 0.0,
+        title: Text(
+          'MARIPOSA MONARCA 3D'.toUpperCase(),
+          style: const TextStyle(
+            fontSize: 15,
+            fontFamily: 'Montserrat',
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              showSearch(context: context, delegate: NegociosSearchDelegate());
+            },
+          ),
+        ],
+      ),
+      body: ModelViewer(src: 'assets/mariposa.glb', ar: true));
+
+  /*@override
   Widget build(BuildContext context) {
     return const Scaffold(
         body: ModelViewer(src: 'assets/mariposa.glb', ar: true));
-  }
+  }*/
 }
